56724bdc7fe36ef9158baa6afd28ac40c6aeb533
testtube123 <32375907+testtube123@users.noreply.github.com>
2017-09-28 23:24:38 -0400

    Got /proc failsafe working

--- mountproc,sys,anddev.sh
+++ mountproc,sys,anddev.sh
@@ -1,11 +1,25 @@
 #!/bin/bash
 # mount proc,sys and dev with a script.Made to make my life easier
-# also chroots (Work in Progress)
+echo "Running ....." 
+if [[ $(findmnt -M "/mnt/gentoo/proc") ]]
+then
+echo " /proc OK"
+else 
+echo "/proc Fail!"
+exit 1
+fi
+
+if [[ $(findmnt -M "/mnt/gentoo/proc") ]] #Checks to see if Proc can by found
+then
+echo " /proc OK" # /Proc Has been found
 mount -t proc /proc /mnt/gentoo/proc
+else 
+echo "Is /Proc Mounted?" # /Proc can't be found
+exit 1 #exit with error 
+fi
 mount --rbind /sys /mnt/gentoo/sys
 mount --make-rslave /mnt/gentoo/sys
 mount --rbind /dev /mnt/gentoo/dev
 mount --make-rslave /mnt/gentoo/dev
-#chroot /mnt/gentoo /bin/bash
-#source /etc/profile
-#export PS1="(chroot) $PS1"
+echo "Exiting ......"
+exit 0
