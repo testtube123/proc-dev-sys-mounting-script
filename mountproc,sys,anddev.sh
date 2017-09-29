dc57e05c59298306591fe7268042b1e9407c7a00
testtube123 <32375907+testtube123@users.noreply.github.com>
2017-09-29 11:16:01 -0400

    /proc failsafe still wrks,but not sure if /sys failsafe works yet

--- mountproc,sys,anddev.sh
+++ mountproc,sys,anddev.sh
@@ -1,25 +1,42 @@
 #!/bin/bash
 # mount proc,sys and dev with a script.Made to make my life easier
-echo "Running ....." 
-if [[ $(findmnt -M "/mnt/gentoo/proc") ]]
+	echo "Running ....." 
+	if [[ $(findmnt -M "/mnt/gentoo/proc") ]]
 then
-echo " /proc OK"
+	echo " /proc OK"
 else 
-echo "/proc Fail!"
+	echo "/proc Fail!"
 exit 1
-fi
+	fi
 
-if [[ $(findmnt -M "/mnt/gentoo/proc") ]] #Checks to see if Proc can by found
+	if [[ $(findmnt -M "/mnt/gentoo/proc") ]] #Checks to see if Proc can by found
 then
-echo " /proc OK" # /Proc Has been found
+	echo " /proc OK" # /Proc can be found
 mount -t proc /proc /mnt/gentoo/proc
 else 
-echo "Is /Proc Mounted?" # /Proc can't be found
+	echo "Is /proc Mounted?" # /Proc can't be found
 exit 1 #exit with error 
-fi
+	fi
+
+	if [[ $(findmnt -M	"/mnt/gentoo/sys") ]]
+then
+	echo " /sys OK" #/Sys can be found
+else
+	echo "/sys Fail!" #/Sys can't be found
+exit 1 #exit with error
+	fi
+
+	if [[	$(findmnt -M "/mnt/gentoo/sys")	]]
+then
+	echo "/sys OK" #/sys can be found
 mount --rbind /sys /mnt/gentoo/sys
 mount --make-rslave /mnt/gentoo/sys
+else
+	echo "Is /sys Mounted?"
+exit 1
+	fi
 mount --rbind /dev /mnt/gentoo/dev
 mount --make-rslave /mnt/gentoo/dev
-echo "Exiting ......"
+	echo "Exiting ......"
 exit 0
+
