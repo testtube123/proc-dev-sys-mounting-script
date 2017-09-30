f3547402ca9d88c726797a93438dac1f0364d45c
testtube123 <32375907+testtube123@users.noreply.github.com>
2017-09-29 23:18:59 -0400

    Ident style changed abit,and addd a few comments.

--- mountproc,sys,anddev.sh
+++ mountproc,sys,anddev.sh
@@ -1,42 +1,41 @@
 #!/bin/bash
 # mount proc,sys and dev with a script.Made to make my life easier
-	echo "Running ....." 
+echo "Running ....." 
 	if [[ $(findmnt -T "/mnt/gentoo/proc") ]]
-then
-	echo " /proc OK"
-else 
-	echo "/proc Fail!"
-exit 1
+	then
+echo " /proc OK" # will display if proc is found
+	else 
+echo "/proc Fail!" # will display if proc can not be found
+	exit 1 # exit with error
 	fi
 
 	if [[ $(findmnt -T "/mnt/gentoo/proc") ]] #Checks to see if Proc can by found
-then
-	echo " /proc OK" # /Proc can be found
-mount -t proc /proc /mnt/gentoo/proc
-else 
-	echo "Is /proc Mounted?" # /Proc can't be found
+	then
+echo " /proc OK" # /Proc can be found
+mount -t proc /proc /mnt/gentoo/proc #mounts /proc
+	else 
+echo "Is /proc Mounted?" # /Proc can't be found
 exit 1 #exit with error 
-	fi
+	fi 
 
 	if [[ $(findmnt -T	"/mnt/gentoo/sys") ]]
-then
-	echo " /sys OK" #/Sys can be found
-else
-	echo "/sys Fail!" #/Sys can't be found
+	then
+echo " /sys OK" #/Sys can be found
+	else
+echo "/sys Fail!" #/Sys can't be found
 exit 1 #exit with error
 	fi
 
 	if [[	$(findmnt -T "/mnt/gentoo/sys")	]]
-then
-	echo "/sys OK" #/sys can be found
+	then
+echo "/sys OK" #/sys can be found
 mount --rbind /sys /mnt/gentoo/sys
 mount --make-rslave /mnt/gentoo/sys
-else
-	echo "Is /sys Mounted?"
-exit 1
+	else
+echo "Is /sys Mounted?" 
+	exit 1
 	fi
 mount --rbind /dev /mnt/gentoo/dev
 mount --make-rslave /mnt/gentoo/dev
-	echo "Exiting ......"
-exit 0
-
+echo "Exiting ......"
+	exit 0
