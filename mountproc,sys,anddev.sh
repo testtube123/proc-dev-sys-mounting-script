d8ccc2ab779bfd967775a7032c447e7ce2198a19
testtube123 <32375907+testtube123@users.noreply.github.com>
2017-10-03 15:12:57 -0400

    Update mountproc,sys,anddev.sh

--- mountproc,sys,anddev.sh
+++ mountproc,sys,anddev.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # mount proc,sys and dev with a script.Made to make my life easier
 echo "Running ....." 
-	if [[ $(findmnt -T "/mnt/gentoo/proc") ]]
+	if [ -d mnt/gentoo/proc ]
 	then
 echo " /proc OK" # will display if proc is found
 	else 
@@ -9,7 +9,7 @@ echo "/proc Fail!" # will display if proc can not be found
 	exit 1 # exit with error
 	fi
 
-	if [[ $(findmnt -T "/mnt/gentoo/proc") ]] #Checks to see if Proc can by found
+	if [ -d /mnt/gentoo/proc ] #Checks to see if Proc can by found
 	then
 echo " /proc OK" # /Proc can be found
 mount -t proc /proc /mnt/gentoo/proc #mounts /proc
@@ -18,7 +18,7 @@ echo "Is /proc Mounted?" # /Proc can't be found
 	exit 1 #exit with error 
 	fi 
 
-	if [[ $(findmnt -T	"/mnt/gentoo/sys") ]]
+	if [ -d  /mnt/gentoo/sys ]
 	then
 echo " /sys OK" #/Sys can be found
 	else
@@ -26,7 +26,7 @@ echo "/sys Fail!" #/Sys can't be found
 	exit 1 #exit with error
 	fi
 
-	if [[	$(findmnt -T "/mnt/gentoo/sys")	]]
+	if [	-d	/mnt/gentoo/sys	]
 	then
 echo "/sys OK" #/sys can be found
 mount --rbind /sys /mnt/gentoo/sys
@@ -36,7 +36,7 @@ echo "Is /sys Mounted?"
 	exit 1
 	fi
 
-	if [[	$(findmnt -T "/mnt/gentoo/dev")	]]
+	if [	-d /mnt/gentoo/dev	]
 	then
 echo "/dev OK" #/dev can be found
 	else
@@ -44,7 +44,7 @@ echo "/dev Fail!" #/dev can't be found
 	exit 1 #exit with error
 	fi
 	
-	if	[[ $(findmnt -T "/mnt/gentoo/dev") ]]
+	if	[ -d /mnt/gentoo/dev	]
 		then
 echo "/dev OK" #/dev can be found		
 mount --rbind /dev /mnt/gentoo/dev
